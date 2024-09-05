75d49aa48b38ec3342211f29e262f7e58a34b219
VRomanenko04 <vovasan2004@gmail.com>
2024-09-05 16:02:58 +0300

    Improved the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,29 @@
+'use client';
 import React from 'react';
 import styles from './Header.module.scss';
 import Link from 'next/link';
-
+import Image from 'next/image';
+import Logo_bw from '@/assets/UCANPOVER_B&W_LOGO.svg';
+import Logo_color from '@/assets/UCANPOVER_COLORED_LOGO.svg';
+import TelephoneIcon from '@/assets/Telephone_icon.svg';
+import TelephoneIconBlack from '@/assets/Telephone_icon_black.svg';
+import { usePathname } from 'next/navigation';
 
 const Header = () => {
+    const pathname = usePathname();
+
     return (
         <nav className={styles.navbar}>
-            <Link className={styles.logo} href='/'>Logo</Link>
+            <Link className={styles.logo} href='/'>
+                <Image src={pathname === '/' ? Logo_bw : Logo_color} alt='ucan power logo'/>
+            </Link>
             <ul className={styles.list}>
-                <li><Link className={styles.link} href='/'>МОДЕЛЬНИЙ РЯД</Link></li>
-                <li><Link className={styles.link} href='/'>НАШІ КОНТАКТИ</Link></li>
+                <li><Link className={pathname === '/' ? styles.link : styles.black__link} href='/'>ПРО НАС</Link></li>
+                <li><Link className={pathname === '/' ? styles.link : styles.black__link} href='/'>МОДЕЛЬНИЙ РЯД</Link></li>
+                <li className={styles.telephone__container}>
+                    <Image src={pathname === '/' ? TelephoneIcon: TelephoneIconBlack} alt='telephone icon'/>
+                    <a className={pathname === '/' ? styles.telephone: styles.telephone__black} href="tel:0507771379">0507771379</a>
+                </li>
             </ul>
         </nav>
     )
