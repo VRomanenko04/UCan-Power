478b9676d3e6ad5ec93db4585d8cddea3bd8005d
VRomanenko04 <vovasan2004@gmail.com>
2024-09-05 16:14:49 +0300

    Improved the header of the main page

--- src/components/HomeHead/HomeHead.tsx
+++ src/components/HomeHead/HomeHead.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import styles from './HomeHead.module.scss';
 import Image from 'next/image';
 import HomeHeader from '@/assets/ucan-power_header.png';
+import UHome from '@/assets/ucan-power_UHome.png';
+import UHB from '@/assets/ucan-power_UHB.png';
 
 const HomeHead = () => {
     return (
         <section className={styles.container}>
             <Image src={HomeHeader} alt='Домівка зі світлом у блекаут' className={styles.image}/>
+            <div className={styles.items__container}>
+                <Image src={UHome} alt='UHome установка' />
+                <Image src={UHB} alt='UHB-50AH установка' />
+            </div>
         </section>
     )
 }
