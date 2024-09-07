ed40833b58d07e295955f4b73dc249d63c6cec91
VRomanenko04 <vovasan2004@gmail.com>
2024-09-07 17:03:19 +0300

    Made a section of individual modules

---
+++ src/components/HomeModules/HomeModules.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './HomeModules.module.scss';
+import Link from 'next/link';
+import Image from 'next/image';
+import Arrow from '@/assets/UCan-power_arrow.svg';
+import { products } from '@/products';
+import ProductBlock from '../ProductBlock/ProductBlock';
+
+
+const HomeModules = () => {
+    return (
+        <section className={styles.wrapper}>
+            <div className={styles.container}>
+                <Link href='/' className={styles.title}>ОКРЕМІ МОДУЛІ <Image src={Arrow} alt='arrow'/></Link>
+                <section className={styles.products__container}>
+                    {products.modules.slice(0, 7).map((product) => (
+                        <ProductBlock 
+                            name={product.name}
+                            description={product.description}
+                            image={product.image}
+                            price={product.price}
+                        />
+                    ))}
+                </section>
+            </div>
+        </section>
+    )
+}
+
+export default HomeModules;
