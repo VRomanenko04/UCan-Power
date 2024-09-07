7f49926b66db6efc4808e69c5e5e2293e8ecad5f
VRomanenko04 <vovasan2004@gmail.com>
2024-09-07 18:42:20 +0300

    Added a footer to the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Commissioner } from "next/font/google";
 import "../styles/globals.scss";
 import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
 const commissioner = Commissioner({ subsets: ["cyrillic"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={commissioner.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
