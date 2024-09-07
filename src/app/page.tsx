e65cd908023215b329dbb3582eb95fb4252ba33d
VRomanenko04 <vovasan2004@gmail.com>
2024-09-07 17:03:32 +0300

    Added new sections to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import HomeAboutUs from '@/components/HomeAboutUs/HomeAboutUs';
 import HomeAdvantages from '@/components/HomeAdvantages/HomeAdvantages';
 import HomeContactForm from '@/components/HomeContactForm/HomeContactForm';
 import HomeHead from '@/components/HomeHead/HomeHead';
+import HomeModules from '@/components/HomeModules/HomeModules';
 import HomeOfficialPartner from '@/components/HomeOfficialPartner/HomeOfficialPartner';
+import HomeReadyOptions from '@/components/HomeReadyOptions/HomeReadyOptions';
 import React from 'react';
 
 
@@ -12,6 +14,8 @@ const HomePage = () => {
       <HomeHead />
       <HomeOfficialPartner />
       <HomeAdvantages />
+      <HomeModules />
+      <HomeReadyOptions />
       <HomeAboutUs />
       <HomeContactForm />
     </main>
