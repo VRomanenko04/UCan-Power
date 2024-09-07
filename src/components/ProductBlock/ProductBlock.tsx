cf353b09f3b992adf726db633ebd63c8a022896c
VRomanenko04 <vovasan2004@gmail.com>
2024-09-08 01:32:11 +0300

    Completely redesigned the product block for mobile devices

--- src/components/ProductBlock/ProductBlock.tsx
+++ src/components/ProductBlock/ProductBlock.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
 import styles from './ProductBlock.module.scss';
+import { useMediaQuery } from 'react-responsive';
 import Image, { StaticImageData } from 'next/image';
 import Link from 'next/link';
 
@@ -11,10 +13,18 @@ type ProductBlockProps = {
 }
 
 const ProductBlock = (props: ProductBlockProps) => {
-    return (
+    const [isMounted, setIsMounted] = useState(false);
+    
+    const isMobile = useMediaQuery({ query: '(max-width: 800px)' });
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    const content = (
         <div className={styles.container}>
             <div className={styles.image__container}>
-                <Image className={`${styles.image} ${!props.price ? styles.long__image: ''}`} src={props.image} alt={props.description}/>
+                <Image className={`${styles.image} ${!props.price ? styles.long__image : ''}`} src={props.image} alt={props.description} />
             </div>
             <div className={styles.title}>
                 <h6>{props.name}</h6>
@@ -23,7 +33,19 @@ const ProductBlock = (props: ProductBlockProps) => {
             <p className={styles.description}>{props.description}</p>
             <Link className={styles.btn} href='/models'>Дізнатись більше</Link>
         </div>
-    )
+    );
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return isMobile ? (
+        <Link href='/models' className={styles.mobile__link}>
+            {content}
+        </Link>
+    ) : (
+        content
+    );
 }
 
 export default ProductBlock;
