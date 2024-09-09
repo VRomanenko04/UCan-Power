1a926ccd0999c3da57ff08284bf1dba2baed0a63
VRomanenko04 <vovasan2004@gmail.com>
2024-09-09 22:42:03 +0300

    Wrote a dynamic product page

---
+++ src/app/models/[name]/page.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { products } from '@/products';
+import styles from '@/styles/ProductPage.module.scss';
+import { StaticImageData } from 'next/image';
+import LinksString from '@/components/LinksString/LinksString';
+
+type ProductPageProps = {
+    params: {
+        name: string
+    }
+}
+
+type ProductObject = {
+    name: string;
+    searchTag: string;
+    description: string;
+    image: StaticImageData;
+    price?: string
+}
+
+
+const findProductByName = (name: string): ProductObject | undefined => {
+    const decodedName = decodeURIComponent(name);
+
+    // Ищем в массиве ready_solutions
+    let product = products.ready_solutions.find(product => product.searchTag === decodedName);
+    
+    // Если не найден в ready_solutions, ищем в модулях
+    if (!product) {
+        product = products.modules.find(product => product.searchTag === decodedName);
+    }
+
+    return product;
+}
+
+
+const ProductPage = ({ params: {name} }: ProductPageProps) => {
+    const product = findProductByName(name);
+
+    if (product) {
+        return (
+            <main className={styles.wrapper}>
+                <LinksString description={product.description}/>
+            </main>
+        )
+    }
+}
+
+export default ProductPage;
