a08553d3539f0edba7bb4bfa9b9736631fa3dca6
VRomanenko04 <vovasan2004@gmail.com>
2024-09-11 12:48:21 +0300

    Adapted the characteristics for mobile devices

--- src/components/ProductCharacteristics/ProductCharacteristics.tsx
+++ src/components/ProductCharacteristics/ProductCharacteristics.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
 import styles from './ProductCharacteristics.module.scss';
 import { products } from '@/products';
+import { useMediaQuery } from 'react-responsive';
 
 type Props = {
     searchTag: string
 }
 
 const ProductCharacteristics = (props: Props) => {
+    const [isMounted, setIsMounted] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+    
+    const isMobile = useMediaQuery({ query: '(max-width: 900px)' });
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+    
     const product = products.modules.find(module => module.searchTag === props.searchTag);
 
     if (!product) {
@@ -19,34 +30,84 @@ const ProductCharacteristics = (props: Props) => {
     const firstColumnEntries = characteristicsEntries.slice(0, 16);
     const secondColumnEntries = characteristicsEntries.slice(16);
 
-    return (
-        <section className={styles.wrapper}>
-            <div className={styles.container}>
-                {Object.keys(characteristics).length > 0 && (
-                    <ul className={styles.first__column}>
-                        <h6 className={styles.title}>Характеристики</h6>
-                        {firstColumnEntries.map(([key, value]) => (
+    const firstPartColumn = characteristicsEntries.slice(0, 5);
+    const secondPartColumn = characteristicsEntries.slice(5);
+
+
+    const DesktopContent = (
+        <div className={styles.container}>
+            {Object.keys(characteristics).length > 0 && (
+                <ul className={styles.first__column}>
+                    <h6 className={styles.title}>Характеристики</h6>
+                    {firstColumnEntries.map(([key, value]) => (
+                        <li className={styles.string} key={key}>{key} - {value as string}</li>
+                    ))}
+                </ul>
+            )}
+            <div>
+                {secondColumnEntries.length > 0 && (
+                    <ul className={styles.second__column__additional}>
+                        <h6 className={styles.title}>Доп. характеристики</h6>
+                        {secondColumnEntries.map(([key, value]) => (
                             <li className={styles.string} key={key}>{key} - {value as string}</li>
                         ))}
                     </ul>
                 )}
-                <div>
-                    {secondColumnEntries.length > 0 && (
-                        <ul className={styles.second__column__additional}>
-                            <h6 className={styles.title}>Доп. характеристики</h6>
-                            {secondColumnEntries.map(([key, value]) => (
+                <ul className={styles.second__column}>
+                    <h6 className={styles.title}>Фізичні характеристики</h6>
+                    {Object.entries(physicCharacteristics).map(([key, value]) => (
+                            <li className={styles.string} key={key}>{key} - {value as string}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+
+    const MobileContent = (
+        <div className={styles.container}>
+            <div className={styles.active__part}>
+                {Object.keys(characteristics).length > 0 && (
+                    <ul className={styles.list}>
+                        <div >
+                            <h6 className={styles.title}>Характеристики</h6>
+                            {firstPartColumn.map(([key, value]) => (
                                 <li className={styles.string} key={key}>{key} - {value as string}</li>
                             ))}
-                        </ul>
-                    )}
-                    <ul className={styles.second__column}>
-                        <h6 className={styles.title}>Фізичні характеристики</h6>
-                        {Object.entries(physicCharacteristics).map(([key, value]) => (
-                                <li className={styles.string} key={key}>{key} - {value as string}</li>
+                        </div>
+                    </ul>
+                )}
+            </div>
+            <div className={`${styles.hidden__part} ${isOpen ? styles.show : ''} ${firstPartColumn.length === 0 ? styles.show : ''}`}>
+                {secondPartColumn.length > 0 && (
+                    <ul className={styles.list}>
+                        <h6 className={styles.title}>Доп. характеристики</h6>
+                        {secondPartColumn.map(([key, value]) => (
+                            <li className={styles.string} key={key}>{key} - {value as string}</li>
                         ))}
                     </ul>
-                </div>
+                )}
+                <ul className={styles.list}>
+                    <h6 className={styles.title}>Фізичні характеристики</h6>
+                    {Object.entries(physicCharacteristics).map(([key, value]) => (
+                            <li className={styles.string} key={key}>{key} - {value as string}</li>
+                    ))}
+                </ul>
             </div>
+            {firstPartColumn.length > 0 && (
+                <div className={styles.show__btn} onClick={() => setIsOpen(prev => !prev)}>
+                    <p className={styles.btn}>{!isOpen ? 'Показати всі' : 'Сховати всі'} <p className={`${!isOpen ? styles.arrow : styles.arrow__up}`}>&gt;</p></p>
+                </div>
+            )}
+        </div>
+    )
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return (
+        <section className={styles.wrapper}>
+            {isMobile ? MobileContent : DesktopContent}
         </section>
     )
 }
