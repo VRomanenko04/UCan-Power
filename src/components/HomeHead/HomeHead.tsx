fa4e7308a5c9a0a400fc1e234b4da95cec06ce24
VRomanenko04 <vovasan2004@gmail.com>
2024-09-11 13:02:15 +0300

    Made corrections from the customer

--- src/components/HomeHead/HomeHead.tsx
+++ src/components/HomeHead/HomeHead.tsx
@@ -7,6 +7,7 @@ import ModileHomeHeader from '@/assets/ucanpowerbg_mobilka.webp';
 import UHome from '@/assets/ucan-power_UHome.png';
 import UHB from '@/assets/ucan-power_UHB.png';
 import { useMediaQuery } from 'react-responsive';
+import Link from 'next/link';
 
 const HomeHead = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -22,7 +23,7 @@ const HomeHead = () => {
             <Image src={ !isMobile ? HomeHeader : ModileHomeHeader} alt='Домівка зі світлом у блекаут' className={styles.image}/>
             <div className={styles.text__container}>
                 <h1>UHome та  UHB для вашого <br /> дому, бізнесу, підприемства</h1>
-                <p>Дізнатись більше&gt;</p>
+                <Link className={styles.link} href='/models'>Дізнатись більше&gt;</Link>
             </div>
             <div className={styles.items__container}>
                 <Image className={styles.item} src={UHome} alt='UHome установка' />
