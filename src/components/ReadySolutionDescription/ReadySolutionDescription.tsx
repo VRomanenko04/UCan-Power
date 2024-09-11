a90ad3d246747a0399cb7ce513ed2843b410793b
VRomanenko04 <vovasan2004@gmail.com>
2024-09-11 22:38:44 +0300

    Added the ability to download documentation

--- src/components/ReadySolutionDescription/ReadySolutionDescription.tsx
+++ src/components/ReadySolutionDescription/ReadySolutionDescription.tsx
@@ -3,6 +3,7 @@ import styles from './ReadySolutionDescription.module.scss';
 import Image from 'next/image';
 import UHome_image from '@/assets/UHome_presentation.svg';
 import UHB_image from '@/assets/UHB_presentation.svg';
+import DownloadButton from '../DownloadButton/DownloadButton';
 
 type Props = {
     name: string
@@ -14,9 +15,12 @@ const ReadySolutionDescription = (props: Props) => {
             <div className={styles.container}>
                 {props.name === 'UHome All in One ESS' && <p className={styles.text}><strong>UHome All in One</strong> — це готове рішення для накопичення сонячної та електричної енергії, яке об’єднує інвертор, зарядний пристрій, джерело безперебійного живлення та батарею в підготовлену до встановлення систему для більш простого та швидкого встановлення, що скорочує час встановлення до 50%.</p>}
                 {props.name === 'UHB 50Ah' && <p className={styles.text}><strong>UHB-50Ah</strong> — це високовольтна батарея, яка пропонує кілька варіантів накопичення енергії завдяки розширюваній модульній конструкції що спрощує установку, експлуатацію та технічне обслуговування за допомогою кількох розумних функцій.</p>}
-                <a className={styles.telephone} href="tel:0507771379">
-                    <div className={styles.btn}>Зв’язатися з нами</div>
-                </a>
+                <div className={styles.btn__container}>
+                    <a className={styles.telephone} href="tel:0507771379">
+                        <div className={styles.btn}>Зв’язатися з нами</div>
+                    </a>
+                    <DownloadButton />
+                </div>
             </div>
             <div className={styles.presentation__container}>
                 {props.name === 'UHome All in One ESS' && <Image className={styles.image} src={UHome_image} alt='UHome презентаційне фото'/>}
