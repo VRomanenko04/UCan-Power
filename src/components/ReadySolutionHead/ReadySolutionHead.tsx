fbfe40494b5e1dee100a4e124d2a8c96c09e35b8
VRomanenko04 <vovasan2004@gmail.com>
2024-09-11 20:52:20 +0300

    Wrote the header block of the ready-made solutions page

---
+++ src/components/ReadySolutionHead/ReadySolutionHead.tsx
@@ -0,0 +1,27 @@
+'use client';
+import React from 'react';
+import styles from './ReadySolutionHead.module.scss';
+import Image from 'next/image';
+import { StaticImageData } from 'next/image';
+
+type ReadySolutionHeadProps = {
+    name: string
+    image_big: StaticImageData
+    image_mid: StaticImageData
+    image_small: StaticImageData
+}
+
+const ReadySolutionHead = (props: ReadySolutionHeadProps) => {
+    return (
+        <section className={styles.wrapper}>
+            <h2 className={styles.title}>{props.name}</h2>
+            <div className={styles.container}>
+                <Image src={props.image_mid} alt='Конвертор з батареєю середній'/>
+                <Image src={props.image_big} alt='Конвертор з батареєю великий'/>
+                <Image src={props.image_small} alt='Конвертор з батареєю маленький'/>
+            </div>
+        </section>
+    )
+}
+
+export default ReadySolutionHead;
