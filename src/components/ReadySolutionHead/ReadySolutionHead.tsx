2eeaf0aa929f74af5d90221a0c212472a46b865d
VRomanenko04 <vovasan2004@gmail.com>
2024-09-12 01:07:25 +0300

    Adapted the head of the ready-made solutions page for mobile devices

--- src/components/ReadySolutionHead/ReadySolutionHead.tsx
+++ src/components/ReadySolutionHead/ReadySolutionHead.tsx
@@ -16,9 +16,9 @@ const ReadySolutionHead = (props: ReadySolutionHeadProps) => {
         <section className={styles.wrapper}>
             <h2 className={styles.title}>{props.name}</h2>
             <div className={styles.container}>
-                <Image src={props.image_mid} alt='Конвертор з батареєю середній'/>
-                <Image src={props.image_big} alt='Конвертор з батареєю великий'/>
-                <Image src={props.image_small} alt='Конвертор з батареєю маленький'/>
+                <Image className={styles.image} src={props.image_mid} alt='Конвертор з батареєю середній'/>
+                <Image className={styles.image} src={props.image_big} alt='Конвертор з батареєю великий'/>
+                <Image className={styles.image} src={props.image_small} alt='Конвертор з батареєю маленький'/>
             </div>
         </section>
     )
