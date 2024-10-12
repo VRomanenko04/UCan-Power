569360fe9c9393264006959347280f0225298649
VRomanenko04 <vovasan2004@gmail.com>
2024-10-12 21:09:05 +0300

    Made a server models page

--- src/app/models/page.tsx
+++ src/app/models/page.tsx
@@ -1,41 +1,14 @@
-'use client';
-import React, { useRef } from 'react';
+import React from 'react';
 import styles from './ModelsPage.module.scss';
-import ModelsNavigation from '@/components/ModelsNavigation/ModelsNavigation';
-import ModelsPageBlock from '@/components/ModelsPageBlock/ModelsPageBlock';
+import ModelsPageContainer from '@/components/ModelsPageContainer';
 
 
 const ModelsPage = () => {
-    const invertersRef = useRef<HTMLDivElement | null>(null);
-    const batteriesRef = useRef<HTMLDivElement | null>(null);
-    const standsRef = useRef<HTMLDivElement | null>(null);
-    const solutionsRef = useRef<HTMLDivElement | null>(null);
-
-    const scrollToSection = (sectionRef: React.RefObject<HTMLDivElement>) => {
-        if (sectionRef.current) {
-            sectionRef.current.scrollIntoView({ behavior: 'smooth' });
-        }
-    };
 
     return (
         <main className={styles.wrapper}>
             <h2 className={styles.title}>Модельний ряд</h2>
-            <ModelsNavigation 
-                scrollTo={scrollToSection}
-                invertersRef={invertersRef}
-                batteriesRef={batteriesRef}
-                standsRef={standsRef}
-                solutionsRef={solutionsRef}
-            />
-            <ModelsPageBlock blockRef={invertersRef} title='Інвертори' tag='Інвертор'/>
-            <ModelsPageBlock blockRef={batteriesRef} title='Батареї' tag='Батарея'/>
-            <ModelsPageBlock blockRef={standsRef} title='Підставки' tag='Підставка'/>
-            <ModelsPageBlock blockRef={solutionsRef} title='Готові рішення' tag='ready_solutions'/>
-
-            {/*
-            <BatteriesBlock />
-            <StandsBlock />
-            <SolutionsBlock /> */}
+            <ModelsPageContainer />
         </main>
     )
 }
