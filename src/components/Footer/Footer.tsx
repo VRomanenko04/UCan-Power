d1d4c5a2388bdc1042ab21d9d6ea86fde73b4c5f
VRomanenko04 <vovasan2004@gmail.com>
2024-10-12 21:03:10 +0300

    Changed alt

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
                 <Image className={styles.logo} src={Logo} alt='UCan Power colored logo'/>
             </Link>
             <div className={styles.info}>
-                <a className={`${styles.link} ${styles.telephone__link}`} href="tel:0507771379"><Image className={styles.telephone} src={TelephoneIconBlack} alt='telephone icon'/>0507771379</a>
+                <a className={`${styles.link} ${styles.telephone__link}`} href="tel:0507771379"><Image className={styles.telephone} src={TelephoneIconBlack} alt='Телефон '/>0507771379 </a>
                 <a className={styles.link} href="mailto:ucanpower@ukr.net">ucanpower@ukr.net</a>
                 <p>м.Харків, вул.Клочківська 174</p>
             </div>
